1d09aeb7e63f936ef1c300ba531f0170b1323292
Mark Miller <fmdesign943@gmail.com>
2017-09-18 20:55:37 -0500

    updated depen

--- src/components/Header/header.title.tsx
+++ src/components/Header/header.title.tsx
@@ -4,7 +4,7 @@ export default class HeaderTitle extends React.Component {
 
   render() {
     return (
-      <h1>React Typescript and Webpack Scaffold</h1>
+      <button className='button is-primary'>Button</button>
     )
   }
 
