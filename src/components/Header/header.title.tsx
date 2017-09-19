58874cd99112e13b0b3dca67fac41c3cf084156b
Mark Miller <markmiller@Marks-MacBook-Pro.local>
2017-09-18 20:56:24 -0500

    updated

--- src/components/Header/header.title.tsx
+++ src/components/Header/header.title.tsx
@@ -1,6 +1,14 @@
 import * as React from 'react';
+import * as mob from 'mobx';
 
 export default class HeaderTitle extends React.Component {
+  public Todo() {
+    const id = Math.random()
+    mob.extendObservable(this, {
+        title: "",
+        finished: false
+    })
+}
 
   render() {
     return (
